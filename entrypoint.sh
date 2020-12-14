7f1a3055b566b4c87dade93336ae360ef9fab816
sebastiaanviaene <sebviaene@gmail.com>
2020-12-14 13:39:29 +0100

    fix: add sshpass & password option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,11 @@ set -eu
 TEMP_SSH_PRIVATE_KEY_FILE='../private_key.pem'
 TEMP_SFTP_FILE='../sftp'
 
-printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
+printf "%s" "$5" >$TEMP_SSH_PRIVATE_KEY_FILE
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
-echo 'ssh start'
-ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $5
 echo 'sftp start'
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
-sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+printf "%s" "put -r $6 $7" >$TEMP_SFTP_FILE
+sshpass -p $2 sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $4 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$3
 
 echo 'deploy successful'
 exit 0
