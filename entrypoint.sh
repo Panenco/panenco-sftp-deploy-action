8106e2dbd9c049d5a2a2a3071fa6744edbc27e26
Leslie Jobse <leslie@jobse.be>
2021-12-01 11:31:21 +0100

    fix: extraneous string in secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo 'sftp start'
 printf "%s" "put -r $6 $7" >$TEMP_SFTP_FILE
 sshpass -p $2 sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $4 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$3
 body="{}"
-signature="$(echo -n "$body" | openssl sha1 -hmac "$10")"
+signature="$(echo -n "$body" | openssl sha1 -hmac "$10" -binary | xxd -p)"
 [ ! -z "$9" ] && curl --location --request POST $9 --data-raw "$body" --header 'Content-Type: application/json' --header "X-Hub-Signature:sha1=$signature"
 echo 'deploy successful'
 exit 0
